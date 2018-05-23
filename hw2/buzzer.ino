dcf79382f4b8068ad2cbf2cd21b26cdf79baada5
natalianna <35853146+natalianna@users.noreply.github.com>
2018-05-23 14:58:40 -0700

    Create buzzer.ino

---
+++ hw2/buzzer.ino
@@ -0,0 +1,11 @@
+void setup() {
+ Serial.begin(19200);
+ pinMode(A0, INPUT);
+}
+void loop() {
+ int value;
+ value = analogRead(A0);
+ Serial.print(value);
+ Serial.println(" 0 1023");
+ tone(2, value);
+}
